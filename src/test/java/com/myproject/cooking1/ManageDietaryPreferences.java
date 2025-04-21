7af0cd49b5c7844582088b7b41a186e124edc2c7
Bisan-Abdelrahim <s12217007@stu.najah.edu>
2025-04-22 02:46:16 +0300

    ManageDietaryPreferences feature

---
+++ src/test/java/com/myproject/cooking1/ManageDietaryPreferences.java
@@ -0,0 +1,54 @@
+package com.myproject.cooking1;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+public class ManageDietaryPreferences {
+    @Given("the customer is logged in")
+    public void theCustomerIsLoggedIn() {
+
+    }
+    @When("the customer navigates to the preferences page")
+    public void theCustomerNavigatesToThePreferencesPage() {
+
+    }
+    @When("the customer enters {string} as a dietary preference and {string} as an allergy")
+    public void theCustomerEntersAsADietaryPreferenceAndAsAnAllergy(String string, String string2) {
+
+    }
+    @When("the customer saves the preferences")
+    public void theCustomerSavesThePreferences() {
+
+    }
+    @Then("the system should store the dietary preference as {string}")
+    public void theSystemShouldStoreTheDietaryPreferenceAs(String string) {
+
+    }
+    @Then("the system should store the allergy as {string}")
+    public void theSystemShouldStoreTheAllergyAs(String string) {
+
+    }
+
+    @Given("the customer has already saved preferences")
+    public void theCustomerHasAlreadySavedPreferences() {
+
+    }
+    @When("the customer updates the dietary preference to {string}")
+    public void theCustomerUpdatesTheDietaryPreferenceTo(String string) {
+
+    }
+    @When("removes {string} from the allergy list")
+    public void removesFromTheAllergyList(String string) {
+
+    }
+    @Then("the system should update the preferences accordingly")
+    public void theSystemShouldUpdateThePreferencesAccordingly() {
+
+    }
+    @Then("the system should display the customer's current dietary preferences and allergies")
+    public void theSystemShouldDisplayTheCustomerSCurrentDietaryPreferencesAndAllergies() {
+
+    }
+
+
+
+}
