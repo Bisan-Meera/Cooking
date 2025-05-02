11224e63fc0a27fc499036a9dc0dae7073d7f2b8
Bisan-Abdelrahim <s12217007@stu.najah.edu>
2025-05-03 01:17:22 +0300

    I started with the customize feature testcases

--- MyFeatures/custom_meal_creator.feature
+++ MyFeatures/custom_meal_creator.feature
@@ -1,44 +1,28 @@
-Feature: Custom Meal Creator
-  Customers should be able to build custom meals using available ingredients, with checks for availability and allergies.
+Feature: Custom Meal Creation and Ingredient Validation
+  Customers should be able to create custom meals using available ingredients.
+  The system must validate stock availability and ingredient selection before saving.
 
   Background:
     Given the Ingredients table has current stock levels
     And the user is logged in as a customer
 
-  Scenario: Create custom meal with available ingredients
+  Scenario: Create a custom meal with available ingredients
     Given a customer selects chicken, rice, and broccoli
     When they submit the custom meal request
     Then the system should accept and save the customized order
     And deduct the used ingredient quantities from stock
 
-  Scenario: Ingredient unavailable
+  Scenario: Prevent use of unavailable ingredient
     Given the ingredient "avocado" is currently out of stock
     When a customer tries to add "avocado" to their custom meal
     Then the system should notify "Avocado is unavailable"
-    And suggest an available alternative ingredient
 
-  Scenario: Ingredient conflicts with allergy
-    Given the customer is allergic to dairy
-    When they try to add "cheese" to their custom meal
-    Then the system should block the selection
-    And show a warning "Ingredient conflicts with your allergy: Dairy"
-
-  Scenario Outline: Submit empty custom meal
+  Scenario: Reject custom meal with no ingredients
     Given a customer is on the custom meal creator page
     When they submit the form without selecting any ingredients
     Then the system should show "At least one ingredient must be selected"
 
-    Examples:
-      | UserID |
-      | 1      |
-      | 2      |
-
-  Scenario Outline: Database error while saving custom meal
-    Given a customer selects ingredients for a custom meal
-    When a database error occurs during save
+  Scenario: Handle database error during custom meal save
+    Given a customer selects chicken and rice
+    When a database error occurs during the save
     Then the system should display "Failed to save custom meal due to system error"
-
-    Examples:
-      | UserID |
-      | 1      |
-      | 2      |
--- src/test/java/com/myproject/cooking1/custom_meal_creator.java
+++ src/test/java/com/myproject/cooking1/custom_meal_creator.java
@@ -1,15 +1,10 @@
 package com.myproject.cooking1;
 
-
-import com.myproject.cooking1.entities.TestContext;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-import static junit.framework.TestCase.assertEquals;
-
 public class custom_meal_creator {
-
     @Given("the Ingredients table has current stock levels")
     public void theIngredientsTableHasCurrentStockLevels() {
 
@@ -35,7 +30,6 @@ public class custom_meal_creator {
 
     }
 
-
     @Given("the ingredient {string} is currently out of stock")
     public void theIngredientIsCurrentlyOutOfStock(String string) {
 
@@ -48,27 +42,6 @@ public class custom_meal_creator {
     public void theSystemShouldNotify(String string) {
 
     }
-    @Then("suggest an available alternative ingredient")
-    public void suggestAnAvailableAlternativeIngredient() {
-
-    }
-
-    @Given("the customer is allergic to dairy")
-    public void theCustomerIsAllergicToDairy() {
-
-    }
-    @When("they try to add {string} to their custom meal")
-    public void theyTryToAddToTheirCustomMeal(String string) {
-
-    }
-    @Then("the system should block the selection")
-    public void theSystemShouldBlockTheSelection() {
-
-    }
-    @Then("show a warning {string}")
-    public void showAWarning(String string) {
-
-    }
 
     @Given("a customer is on the custom meal creator page")
     public void aCustomerIsOnTheCustomMealCreatorPage() {
@@ -76,19 +49,15 @@ public class custom_meal_creator {
     }
     @When("they submit the form without selecting any ingredients")
     public void theySubmitTheFormWithoutSelectingAnyIngredients() {
-        // Simulate validation error
-        TestContext.set("lastMessage", "At least one ingredient must be selected");
-    }
-
-
-    @Given("a customer selects ingredients for a custom meal")
-    public void aCustomerSelectsIngredientsForACustomMeal() {
 
     }
-    @When("a database error occurs during save")
-    public void aDatabaseErrorOccursDuringSave() {
-        // Simulate failure to save due to DB error
-        TestContext.set("lastMessage", "Failed to save custom meal due to system error");
+    @Given("a customer selects chicken and rice")
+    public void aCustomerSelectsChickenAndRice() {
+
+    }
+    @When("a database error occurs during the save")
+    public void aDatabaseErrorOccursDuringTheSave() {
+
     }
 
 
