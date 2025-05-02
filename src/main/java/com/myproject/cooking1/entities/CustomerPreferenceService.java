7e5ebf55be6bd79f0928c64d70840471e5432ba3
Bisan-Abdelrahim <s12217007@stu.najah.edu>
2025-05-03 01:17:57 +0300

    edit a simple part in the customer preferences

---
+++ src/main/java/com/myproject/cooking1/entities/CustomerPreferenceService.java
@@ -0,0 +1,29 @@
+package com.myproject.cooking1.entities;
+
+import com.myproject.cooking1.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CustomerPreferenceService {
+    public static CustomerPreferences getPreferencesByCustomerId(int userId) throws SQLException {
+        String query = "SELECT dietary_preference, allergy FROM CustomerPreferences WHERE user_id = ?";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement stmt = ((Connection) conn).prepareStatement(query)) {
+            stmt.setInt(1, userId);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return new CustomerPreferences(
+                            rs.getString("dietary_preference"),
+                            rs.getString("allergy")
+                    );
+                } else {
+                    return null;
+                }
+            }
+        }
+    }
+}
+
