17c91835c7275b4d0b86f8aa9e42329cd1691205
Bisan-Abdelrahim <s12217007@stu.najah.edu>
2025-05-17 00:40:40 +0300

    ---

--- src/main/java/com/myproject/cooking1/DBConnection.java
+++ src/main/java/com/myproject/cooking1/DBConnection.java
@@ -5,12 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = System.getenv("DB_URL");
-    private static final String USER = System.getenv("DB_USER");
-    private static final String PASSWORD = System.getenv("DB_PASS");
+    private static final String URL = System.getenv("DB_URL") != null ?
+            System.getenv("DB_URL") :
+            "jdbc:postgresql://aws-0-us-west-1.pooler.supabase.com:5432/postgres?sslmode=require";
+    private static final String USER = "postgres.qqdgnbhrovoinqjzrsna";
+    private static final String PASSWORD = "bisan2005";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
+
 
