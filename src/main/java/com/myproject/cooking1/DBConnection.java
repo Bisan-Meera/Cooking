01c356e8e76b9513d66d185190a4b34d69b437fa
Bisan-Abdelrahim <s12217007@stu.najah.edu>
2025-05-16 22:41:27 +0300

    editing the DBConnection

--- src/main/java/com/myproject/cooking1/DBConnection.java
+++ src/main/java/com/myproject/cooking1/DBConnection.java
@@ -9,9 +9,9 @@ public class DBConnection {
     private static final String DEFAULT_USER = "postgres";
     private static final String DEFAULT_PASSWORD = "bisan2005";
 
-    private static final String URL = System.getenv("DB_URL") != null ? System.getenv("DB_URL") : DEFAULT_URL;
-    private static final String USER = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : DEFAULT_USER;
-    private static final String PASSWORD = System.getenv("DB_PASS") != null ? System.getenv("DB_PASS") : DEFAULT_PASSWORD;
+    private static final String URL = System.getenv("SUPABASE_DB_URL") != null ? System.getenv("SUPABASE_DB_URL") : DEFAULT_URL;
+    private static final String USER = System.getenv("SUPABASE_DB_USER") != null ? System.getenv("SUPABASE_DB_USER") : DEFAULT_USER;
+    private static final String PASSWORD = System.getenv("SUPABASE_DB_PASS") != null ? System.getenv("SUPABASE_DB_PASS") : DEFAULT_PASSWORD;
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
