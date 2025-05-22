ebf1f8f27df9a80b2d94c4d802af6bdac7e1a7d0
Bisan-Abdelrahim <s12217007@stu.najah.edu>
2025-05-22 07:25:25 +0300

    to test one feature individually

---
+++ src/test/java/com/myproject/cooking1/OneFeatureTest.java
@@ -0,0 +1,21 @@
+package com.myproject.cooking1;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "MyFeatures/task_assignment.feature",
+        glue = {"com.myproject.cooking1"},
+        monochrome = true,
+        plugin = {
+                "pretty",
+                "json:target/cucumber-report-ingredient.json"
+        }
+)
+
+
+public class OneFeatureTest {
+}
+
