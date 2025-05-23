b74be4c689ed0541b1e4fc159ff1e66df3c6d6dd
Bisan-Abdelrahim <s12217007@stu.najah.edu>
2025-05-23 19:53:47 +0300

    third refactoring

--- src/main/java/com/myproject/cooking1/DBConnection.java
+++ src/main/java/com/myproject/cooking1/DBConnection.java
@@ -9,7 +9,7 @@ public class DBConnection {
             System.getenv("DB_URL") :
             "jdbc:postgresql://aws-0-us-west-1.pooler.supabase.com:5432/postgres?sslmode=require";
     private static final String USER = "postgres.qqdgnbhrovoinqjzrsna";
-    private static final String PASSWORD = "bisan2005";
+    private static final String PASSWORD = System.getenv("DBPASS");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
